97abbff59493e9794afbba273b91c4b9f8a91e7b
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-01-20 18:00:52 +0100

    trie structure

--- merkle-tree/src/patricia.rs
+++ merkle-tree/src/patricia.rs
@@ -1,94 +1,146 @@
 // Patricia Merkle Tree for use in Kairos V0
 
-use std::{collections::HashSet, vec, slice::Chunks, process::Child};
 extern crate alloc;
-use alloc::boxed::Box;
-use digest::typenum::Le;
+use crate::helpers::hash_bytes;
+//use alloc::boxed::Box;
 
-struct Trie{
-	levels: Vec<Option<NodeEnum>>
+#[derive(Clone, PartialEq)]
+struct Root{
+    hash: Option<Vec<u8>>,
+    children: Vec<NodeEnum>,
 }
 
-#[derive(Clone)]
-struct Level{
-	hash: Option<Vec<u8>>,
-	children: Vec<Node>,
+#[derive(Clone, PartialEq)]
+struct Leaf{
+    key: &'static [u8],
+    hash: Option<Vec<u8>>,
+    serialized_data: Option<Vec<u8>>,
 }
 
 #[derive(Clone, PartialEq)]
 struct Node{
     key: &'static [u8],
-	hash: Vec<u8>,
-    children: Option<Vec<Node>>,
-	serialized_data: Option<Vec<u8>>
+	hash: Option<Vec<u8>>,
+    children: Vec<NodeEnum>,
 }
 
-impl Node{
-    fn has_key(&self, key: &[u8]) -> bool {
-        self.key == key
-    }
-}
-
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 enum NodeEnum{
-	Level(Box<Level>),
-	Node(Box<Node>)
+    Node(Node),
+    Leaf(Leaf),
+    Root(Root),
 }
 impl NodeEnum{
-    fn unwrap_as_level(self) -> Level{
-        let level = match self{
-            NodeEnum::Level(level) => {
-                return *level;
-            },
-            NodeEnum::Node(_) => unreachable!("This should never happen!")
-        };
+    fn unwrap_as_node(self) -> Node{
+        match self{
+            NodeEnum::Node(node) =>
+                node,
+            NodeEnum::Leaf(_) => panic!("Tried to unwrap a leaf as node!"),
+            NodeEnum::Root(_) => panic!("Tried to unwrap a root as node!")
+        }
     }
-    
+    fn unwrap_as_leaf(self) -> Leaf{
+        match self{
+            NodeEnum::Leaf(leaf) => 
+                leaf,
+            NodeEnum::Node(_) => panic!("Tried to unwrap a node as leaf!"),
+            NodeEnum::Root(_) => panic!("Tried to unwrap a root as leaf!")
+        }
+    }
+    fn unwrap_as_root(self) -> Root{
+        match self{
+            NodeEnum::Root(root) => 
+                root,
+            NodeEnum::Node(_) => panic!("Tried to unwrap a node as root!"),
+            NodeEnum::Leaf(_) => panic!("Tried to unwrap a leaf as root!")
+        }
+    }
+    fn has_key(&self, key: &[u8]) -> bool {
+        match self{
+            NodeEnum::Root(root) => {
+                unreachable!("This should never happen!");
+            }
+            NodeEnum::Node(node) => {
+                node.key == key
+            }
+            NodeEnum::Leaf(leaf) => {
+                leaf.key == key
+            }
+        }
+    }
+    fn hash(&self) -> Vec<u8>{
+        match self{
+            NodeEnum::Root(root) => {
+                let mut preimage: Vec<u8> = Vec::new();
+                for child in &root.children{
+                    match child{
+                        NodeEnum::Node(node) => {
+                            preimage.append(&mut node.hash.as_ref().unwrap().to_owned());
+                        },
+                        NodeEnum::Leaf(leaf) => {
+                            preimage.append(&mut leaf.hash.as_ref().unwrap().to_owned());
+                        },
+                        NodeEnum::Root(_) => unreachable!("This should never happen!")
+                    }
+                };
+                return hash_bytes(preimage);
+            },
+            NodeEnum::Node(node) => {
+                let mut preimage: Vec<u8> = Vec::new();
+                for child in &node.children{
+                    match child{
+                        NodeEnum::Node(node) => {
+                            preimage.append(&mut node.hash.as_ref().unwrap().to_owned());
+                        },
+                        NodeEnum::Leaf(leaf) => {
+                            preimage.append(&mut leaf.hash.as_ref().unwrap().to_owned());
+                        },
+                        NodeEnum::Root(_) => unreachable!("This should never happen!")
+                    }
+                };
+                return hash_bytes(preimage);
+            },
+            NodeEnum::Leaf(leaf) => {
+                return hash_bytes(leaf.key.to_owned());
+            }
+        }
+    }
 }
 
+trait Appendable{
+    fn append(self, node: NodeEnum);
+}
+
+impl Appendable for Root{
+    fn append(mut self, node: NodeEnum) {
+        self.children.push(node);
+    }
+}
+
+impl Appendable for Node{
+    fn append(mut self, node: NodeEnum) {
+        self.children.push(node);
+    }
+}
 
 #[test]
 fn tests(){
-    // max depth of trie will be 32/2 = 16
-    let trie = Trie{
-        levels: vec![None;16]
-    };
     use crate::helpers::hash_bytes;
     // create a set of 10 transactions
     let mut transactions: Vec<Vec<u8>> = Vec::new();
     for i in 0..10{
         transactions.push(hash_bytes(vec![0,0,i]));
     };
+    // construct a trie root
+    let mut trie_root = NodeEnum::Root(Root { 
+        hash: None, 
+        children: Vec::new()
+    });
+    // insert the set of transactions and update the trie
+    let mut current_node: NodeEnum = trie_root;
     for transaction in transactions{
-        let mut depth: usize = 0;
         for chunk in transaction.chunks(2){
-            let level = match trie.levels[depth].clone(){
-                Some(level) => level,
-                None => {
-                    // return a new level
-                    NodeEnum::Level(Box::new(Level{
-                        hash: None,
-                        children: Vec::new()
-                    }))
-                }
-            }.unwrap_as_level();
-            // insert to level if key does not exist
-            // move on to the next level if key does exist
-            // update all hashs for all affected levels
-
-            let mut is_new: bool = true;
-            for child in level.children{
-                if child.has_key(&chunk){
-                    is_new = false;
-                    break;
-                }
-            }
-            if is_new{
-                // insert the new chunk (as a Node) into the child
-                // don't yet update the hashs, that happens in the end
-                // to find a given node, use the key (Node.has_key())
-            }
-            depth += 1;
+        
         }
     }
 }
